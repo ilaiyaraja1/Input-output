eb7290a39d426bc35ee0c58b8a4602d9b6438ffe
ilaiyaraja1 <ilaiyaraja.bca@gmail.com>
2021-08-22 18:47:27 +0530

    input & output
    now i give here simple input and output.and this programme ask you. your name ,age, braingpower. then they will show your name age  brainpower.,,,

---
+++ src/One/Two.java
@@ -0,0 +1,17 @@
+package One;
+import java.util.*;
+public class Two {
+
+	public static void main(String[] args) {
+	System.out.println("enter your =Name,Age,your Brain power");
+	Scanner sc=new Scanner(System.in);
+    String name=sc.nextLine();  //your name,you will give here String value.
+    int age=sc.nextInt();       //your age,your will give here intiger value.
+    float power=sc.nextFloat(); //your brain power your will give here on foating value.
+    System.out.println("your name is :"+name);
+    System.out.println("your age is:"+age);
+    System.out.println("your brain power is:"+power);
+    
+	}
+
+}
